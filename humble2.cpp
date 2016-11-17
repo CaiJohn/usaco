2d6d2f535445483a3b3d520d60526f398e217582
John@asus <johnzhuohong@gmail.com>
2016-11-16 19:02:38 -0500

    summary

---
+++ humble2.cpp
@@ -0,0 +1,24 @@
+/*
+  ID: remijoh1
+  LANG: C++11
+  TASK: humble
+*/
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+  ifstream fin("humble.in");
+  ofstream fout("humble.out");
+  int K,N,primes[100];
+  fin>>K>>N;
+  for(int i=0;i<k;i++)
+    fin>>primes[i];
+
+  long long gen[100001],fac[100001];
+  int start = 0;
+  gen[0] = 1;
+
+  for(int i=0;i<N;i++){
+    
+  }
